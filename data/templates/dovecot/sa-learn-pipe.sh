813ab01e9d3f456a321c3ba184f26ce749ceebae
kload <kload@kload.fr>
2015-10-31 12:33:06 +0100

    [enh] Replace the email stack by Rspamd/Rmilter

--- data/templates/dovecot/sa-learn-pipe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo /usr/bin/sa-learn $* /tmp/sendmail-msg-$$.txt ;
-echo "$$-start ($*)" >> /tmp/sa-learn-pipe.log ;
-#echo $* > /tmp/sendmail-parms.txt ;
-cat<&0 >> /tmp/sendmail-msg-$$.txt ;
-/usr/bin/sa-learn $* /tmp/sendmail-msg-$$.txt ;
-rm -f /tmp/sendmail-msg-$$.txt ;
-echo "$$-end" >> /tmp/sa-learn-pipe.log ;
-exit 0;
