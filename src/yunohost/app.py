31bedc5fa4aaa8b6ec7b6acbebfa3d74e201217d
Jérôme Lebleu <jerome@maroufle.fr>
2016-04-26 15:56:06 +0200

    [enh] Catch boolean in is_true method of app.py

--- src/yunohost/app.py
+++ src/yunohost/app.py
@@ -1546,11 +1546,17 @@ def is_true(arg):
         Boolean
 
     """
-    true_list = ['yes', 'Yes', 'true', 'True' ]
-    for string in true_list:
-        if arg == string:
-            return True
-    return False
+    if isinstance(arg, bool):
+        return arg
+    elif isinstance(arg, basestring):
+        true_list = ['yes', 'Yes', 'true', 'True' ]
+        for string in true_list:
+            if arg == string:
+                return True
+        return False
+    else:
+        logger.debug('arg should be a boolean or a string, got %r', arg)
+        return True if arg else False
 
 
 def random_password(length=8):
