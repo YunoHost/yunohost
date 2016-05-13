04929f4c8430ad60c3b85416bb44303b1d8b3e90
Laurent Peuch <cortex@worlddomination.be>
2016-05-13 04:03:39 +0200

    [mod] update copyright years

--- src/yunohost/domain.py
+++ src/yunohost/domain.py
@@ -2,7 +2,7 @@
 
 """ License
 
-    Copyright (C) 2013 YunoHost
+    Copyright (C) 2013-2016 YunoHost
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
