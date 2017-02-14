33cf0f92ced6bbe56a2340baa4382dc2483aa5da
Laurent Peuch <cortex@worlddomination.be>
2017-02-14 14:47:41 +0100

    [enh] migrate to pytest assert style

--- src/yunohost/tests/test_settings.py
+++ src/yunohost/tests/test_settings.py
@@ -1,30 +1,10 @@
 import os
 import json
+import pytest
 
 from moulinette.core import MoulinetteError
 
 from yunohost.settings import settings_get, settings_list, _get_settings, settings_set, settings_default, settings_reset, SETTINGS_PATH_OTHER_LOCATION, SETTINGS_PATH
-
-
-def assertEqual(a, b):
-    # == can have different behavior than !=
-    if not (a == b):
-        raise AssertionError("%s != %s" % (a, b))
-
-
-def assertNotEqual(a, b):
-    # == can have different behavior than !=
-    if not (a != b):
-        raise AssertionError("%s != %s" % (a, b))
-
-
-def assertRaise(exception_, func, *args, **kwargs):
-    try:
-        func(*args, **kwargs)
-    except exception_:
-        pass
-    else:
-        raise AssertionError("%s should have raise %s with arguments %s and %s" % (func.__name__, exception_.__name__, args, kwargs))
 
 
 def setup_function(function):
@@ -36,88 +16,102 @@ def teardown_function(function):
 
 
 def test_settings_get_bool():
-    assertEqual(settings_get("example.bool"), {"type": "bool", "value": True, "default": True, "description": "Example boolean option"})
+    assert settings_get("example.bool") == {"type": "bool", "value": True, "default": True, "description": "Example boolean option"}
 
 
 def test_settings_get_int():
-    assertEqual(settings_get("example.int"), {"type": "int", "value": 42, "default": 42, "description": "Example int option"})
+    assert settings_get("example.int") == {"type": "int", "value": 42, "default": 42, "description": "Example int option"}
 
 
 def test_settings_get_string():
-    assertEqual(settings_get("example.string"), {"type": "string", "value": "yolo swag", "default": "yolo swag", "description": "Example string option"})
+    assert settings_get("example.string") == {"type": "string", "value": "yolo swag", "default": "yolo swag", "description": "Example string option"}
 
 
 def test_settings_get_enum():
-    assertEqual(settings_get("example.enum"), {"type": "enum", "value": "a", "default": "a", "description": "Example enum option", "choices": ["a", "b", "c"]})
+    assert settings_get("example.enum") == {"type": "enum", "value": "a", "default": "a", "description": "Example enum option", "choices": ["a", "b", "c"]}
 
 
 def test_settings_get_doesnt_exists():
-    assertRaise(MoulinetteError, settings_get, "doesnt.exists")
+    with pytest.raises(MoulinetteError):
+        settings_get("doesnt.exists")
 
 
 def test_settings_list():
-    assertEqual(settings_list(), _get_settings())
+    assert settings_list() == _get_settings()
 
 
 def test_settings_set():
     settings_set("example.bool", False)
-    assertEqual(settings_get("example.bool")["value"], False)
+    assert settings_get("example.bool")["value"] == False
 
 
 def test_settings_set_int():
     settings_set("example.int", 21)
-    assertEqual(settings_get("example.int")["value"], 21)
+    assert settings_get("example.int")["value"] == 21
 
 
 def test_settings_set_enum():
     settings_set("example.enum", "c")
-    assertEqual(settings_get("example.enum")["value"], "c")
+    assert settings_get("example.enum")["value"] == "c"
 
 
 def test_settings_set_doesexit():
-    assertRaise(MoulinetteError, settings_set, "doesnt.exist", True)
+    with pytest.raises(MoulinetteError):
+        settings_set("doesnt.exist", True)
 
 
 def test_settings_set_bad_type_bool():
-    assertRaise(MoulinetteError, settings_set, "example.bool", 42)
-    assertRaise(MoulinetteError, settings_set, "example.bool", "pouet")
+    with pytest.raises(MoulinetteError):
+        settings_set("example.bool", 42)
+    with pytest.raises(MoulinetteError):
+        settings_set("example.bool", "pouet")
 
 
 def test_settings_set_bad_type_int():
-    assertRaise(MoulinetteError, settings_set, "example.int", True)
-    assertRaise(MoulinetteError, settings_set, "example.int", "pouet")
+    with pytest.raises(MoulinetteError):
+        settings_set("example.int", True)
+    with pytest.raises(MoulinetteError):
+        settings_set("example.int", "pouet")
 
 
 def test_settings_set_bad_type_string():
-    assertRaise(MoulinetteError, settings_set, "example.string", True)
-    assertRaise(MoulinetteError, settings_set, "example.string", 42)
+    with pytest.raises(MoulinetteError):
+        settings_set("example.string", True)
+    with pytest.raises(MoulinetteError):
+        settings_set("example.string", 42)
 
 
 def test_settings_set_bad_value_enum():
-    assertRaise(MoulinetteError, settings_set, "example.enum", True)
-    assertRaise(MoulinetteError, settings_set, "example.enum", "e")
-    assertRaise(MoulinetteError, settings_set, "example.enum", 42)
-    assertRaise(MoulinetteError, settings_set, "example.enum", "pouet")
+    with pytest.raises(MoulinetteError):
+        settings_set("example.enum", True)
+    with pytest.raises(MoulinetteError):
+        settings_set("example.enum", "e")
+    with pytest.raises(MoulinetteError):
+        settings_set("example.enum", 42)
+    with pytest.raises(MoulinetteError):
+        settings_set("example.enum", "pouet")
 
 
 def test_settings_list_modified():
     settings_set("example.int", 21)
-    assertEqual(settings_list()["example.int"]["value"], 21)
+    assert settings_list()["example.int"]["value"] == 21
 
 
 def test_default():
     settings_set("example.int", 21)
-    assertEqual(settings_get("example.int")["value"], 21)
+    assert settings_get("example.int")["value"] == 21
     settings_default("example.int")
-    assertEqual(settings_get("example.int")["value"], settings_get("example.int")["default"])
+    assert settings_get("example.int")["value"] == settings_get("example.int")["default"]
 
 
 def test_settings_default_doesexit():
-    assertRaise(MoulinetteError, settings_default, "doesnt.exist")
+    with pytest.raises(MoulinetteError):
+        settings_default("doesnt.exist")
 
 
 def test_settings_reset_no_yes():
-    assertRaise(MoulinetteError, settings_reset)
+    with pytest.raises(MoulinetteError):
+        settings_reset()
 
 
 def test_reset():
@@ -126,7 +120,7 @@ def test_reset():
     settings_set("example.int", 21)
     settings_set("example.string", "pif paf pouf")
     settings_set("example.enum", "c")
-    assertNotEqual(settings_before, settings_list())
+    assert settings_before != settings_list()
     settings_reset(yes=True)
     if settings_before != settings_list():
         for i in settings_before:
@@ -140,13 +134,13 @@ def test_reset_backup():
     settings_set("example.string", "pif paf pouf")
     settings_set("example.enum", "c")
     settings_after_modification = settings_list()
-    assertNotEqual(settings_before, settings_after_modification)
+    assert settings_before != settings_after_modification
     old_settings_backup_path = settings_reset(yes=True)["old_settings_backup_path"]
 
     for i in settings_after_modification:
         del settings_after_modification[i]["description"]
 
-    assertEqual(settings_after_modification, json.load(open(old_settings_backup_path, "r")))
+    assert settings_after_modification == json.load(open(old_settings_backup_path, "r"))
 
 
 def test_unknown_keys():
@@ -159,4 +153,4 @@ def test_unknown_keys():
     # stimulate a write
     settings_reset(yes=True)
 
-    assertEqual(unknown_setting, json.load(open(unknown_settings_path, "r")))
+    assert unknown_setting == json.load(open(unknown_settings_path, "r"))
