61aaadf45d26880165607193acf395c6609c71fd
Alexandre Aubin <alexandre.aubin@cern.ch>
2017-04-17 02:53:44 +0200

    Adding test for set permissions

--- src/yunohost/tests/test_iohelpers.py
+++ src/yunohost/tests/test_iohelpers.py
@@ -4,36 +4,107 @@ import requests
 import requests_mock
 import glob
 import time
+import pwd
+from stat import *
 
 from moulinette.core import MoulinetteError
 
-from yunohost.io import download_text, download_json
+from yunohost.io import download_text, download_json, set_permissions
 
+# TODO:
 #read_from_file
 #read_from_json
 #write_to_file
 #append_to_file
 #write_to_json
 #remove_file
-#set_permissions
-#download_text
-#download_json
 #run_shell_commands
 
 
+# We define a dummy context with test folders and files
+
+TEST_URL = "https://some.test.url/yolo.txt"
+TMP_TEST_DIR = "/tmp/test_iohelpers"
+TMP_TEST_FILE = "%s/foofile" % TMP_TEST_DIR
+NON_ROOT_USER = "admin"
+NON_ROOT_GROUP = "mail"
+
 def setup_function(function):
-    pass
+    os.system("rm -rf %s" % TMP_TEST_DIR)
+    os.system("mkdir %s" % TMP_TEST_DIR)
+    os.system("touch %s" % TMP_TEST_FILE)
 
 
 def teardown_function(function):
-    pass
+    os.seteuid(0)
+    os.system("rm -rf /tmp/test_iohelpers/")
+
+
+# Helper to try stuff as non-root
+def switch_to_non_root_user():
+    nonrootuser = pwd.getpwnam(NON_ROOT_USER).pw_uid
+    os.seteuid(nonrootuser)
+
+
+###############################################################################
+#   Test permission change                                                    #
+###############################################################################
+
+
+def get_permissions(file_path):
+    return (pwd.getpwuid(os.stat(file_path).st_uid).pw_name,
+	    pwd.getpwuid(os.stat(file_path).st_gid).pw_name,
+            oct(os.stat(file_path)[ST_MODE])[-3:])
+
+
+def test_setpermissions_file():
+
+    # Check we're at the default permissions
+    assert get_permissions(TMP_TEST_FILE) == ("root", "root", "644")
+
+    # Change the permissions
+    set_permissions(TMP_TEST_FILE, NON_ROOT_USER, NON_ROOT_GROUP, 0111)
+    
+    # Check the permissions got changed
+    assert get_permissions(TMP_TEST_FILE) == (NON_ROOT_USER, NON_ROOT_GROUP, "111")
+
+    # Change the permissions again
+    set_permissions(TMP_TEST_FILE, "root", "root", 0777)
+    
+    # Check the permissions got changed
+    assert get_permissions(TMP_TEST_FILE) == ("root", "root", "777")
+ 
+
+def test_setpermissions_file_permissiondenied():
+
+    switch_to_non_root_user()
+    
+    with pytest.raises(MoulinetteError):
+        set_permissions(TMP_TEST_FILE, NON_ROOT_USER, NON_ROOT_GROUP, 0111)
+
+
+def test_setpermissions_file_badfile():
+
+    with pytest.raises(MoulinetteError):
+        set_permissions("/foo/bar/yolo", NON_ROOT_USER, NON_ROOT_GROUP, 0111)
+
+
+def test_setpermissions_file_baduser():
+
+    with pytest.raises(MoulinetteError):
+        set_permissions(TMP_TEST_FILE, "foo", NON_ROOT_GROUP, 0111)
+
+
+def test_setpermissions_file_badgroup():
+
+    with pytest.raises(MoulinetteError):
+        set_permissions(TMP_TEST_FILE, NON_ROOT_USER, "foo", 0111)
 
 
 ###############################################################################
 #   Test download                                                             #
 ###############################################################################
 
-TEST_URL = "https://some.test.url/yolo.txt"
 
 def test_download():
 
@@ -97,5 +168,3 @@ def test_download_json_badjson():
         with pytest.raises(MoulinetteError):
             download_json(TEST_URL)
     
-
-
