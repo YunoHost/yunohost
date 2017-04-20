a16526bce62dd825495449f7b1321de8eace6595
Alexandre Aubin <alexandre.aubin@cern.ch>
2017-04-20 15:21:35 +0200

    Add random delay to app fetchlist cron job

--- src/yunohost/app.py
+++ src/yunohost/app.py
@@ -1933,8 +1933,14 @@ def _install_appslist_fetch_cron():
 
     logger.debug("Installing appslist fetch cron job")
 
+    cron_job = []
+    cron_job.append("#!/bin/bash")
+    # We add a random delay between 0 and 60 min to avoid every instance fetching
+    # the appslist at the same time every night
+    cron_job.append("(sleep $((RANDOM%3600)); yunohost app fetchlist > /dev/null 2>&1) &")
+
     with open(cron_job_file, "w") as f:
-        f.write('#!/bin/bash\n\nyunohost app fetchlist > /dev/null 2>&1\n')
+        f.write('\n'.join(cron_job))
 
     _set_permissions(cron_job_file, "root", "root", 0755)
 
