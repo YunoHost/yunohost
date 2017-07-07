197c8f26d720bfccdbb7f37b4c41487f50592c29
Laurent Peuch <cortex@worlddomination.be>
2017-07-07 15:53:47 +0200

    [mod] DRY

--- src/yunohost/migrations.py
+++ src/yunohost/migrations.py
@@ -43,26 +43,11 @@ def migrations_list(auth):
 
     migrations = {"migrations": []}
 
-    try:
-        import data_migrations
-    except ImportError:
-        # not data migrations present, return empty list
-        return migrations
-
-    migrations_path = data_migrations.__path__[0]
-
-    if not os.path.exists(migrations_path):
-        logger.warn("Can't access migrations files at path %s".format(migrations_path))
-        return migrations
-
-    for migration in filter(lambda x: re.match("^\d+_[a-zA-Z0-9_]+\.py$", x), os.listdir(migrations_path)):
-        migration = migration[:-len(".py")]
+    for migration in _get_migrations_list():
         migrations["migrations"].append({
             "number": migration.split("_", 1)[0],
             "name": migration.split("_", 1)[1],
         })
-
-    migrations["migrations"] = sorted(migrations["migrations"], key=lambda x: x["number"])
 
     return migrations
 
@@ -80,32 +65,27 @@ def migrations_migrate(auth):
 
     migrations = []
 
-    # XXX DRY
-    try:
-        import data_migrations
-    except ImportError:
-        return
-
-    # XXX error handling
-    for migration in filter(lambda x: re.match("^\d+_[a-zA-Z0-9_]+\.py$", x), os.listdir(data_migrations.__path__[0])):
-        number = migration.split("_", 1)[0]
-
+    # loading all migrations that haven't already runned
+    for migration in migrations_list()::
         # skip already runnned migrations
         if state["last_runned_migration"] is not None and\
-           int(number) <= int(state["last_runned_migration"]["number"]):
+           int(migration["number"]) <= int(state["last_runned_migration"]["number"]):
             continue
 
-        migration = migration[:-len(".py")]
         migrations.append({
-            "number": number,
-            "name": migration.split("_", 1)[1],
+            "number": migration["number"],
+            "name": migration["name"]
+            # this is python buildin method to import a module using a name, we use that to import the migration
+            # has a python object so we'll be able to run it in the next lop
             "module": import_module("yunohost.data_migrations.{}".format(migration)), # XXX error handling
         })
 
     migrations = sorted(migrations, key=lambda x: x["number"])
 
+    # run migrations in order
     for migration in migrations:
         logger.info("Running migration {number} {name}...".format(**migration))
+
         migration["module"].MyMigration().migrate() # XXX error handling
 
         state["last_runned_migration"] = {
@@ -125,6 +105,27 @@ def migrations_state():
     else:
         # XXX error handling
         return json.load(open(MIGRATIONS_STATE_PATH))
+
+
+def _get_migrations_list():
+    migrations = []
+
+    try:
+        import data_migrations
+    except ImportError:
+        # not data migrations present, return empty list
+        return migrations
+
+    migrations_path = data_migrations.__path__[0]
+
+    if not os.path.exists(migrations_path):
+        logger.warn("Can't access migrations files at path %s".format(migrations_path))
+        return migrations
+
+    for migration in filter(lambda x: re.match("^\d+_[a-zA-Z0-9_]+\.py$", x), os.listdir(migrations_path)):
+        migrations.append(migration[:-len(".py")])
+
+    return sorted(migrations)
 
 
 class Migration(object):
