64852775a30d0526b9f7133ef7fc16ebafbf18ab
Laurent Peuch <cortex@worlddomination.be>
2017-08-08 08:10:49 +0200

    [enh] I want auth everywhere because those are super security critical actions

--- src/yunohost/ssh.py
+++ src/yunohost/ssh.py
@@ -1,56 +1,55 @@
-# (username list) + root + admin
-def ssh_user_list():
+# (auth, username list) + root + admin
+def ssh_user_list(auth):
     pass
 
 
-def ssh_user_allow_ssh(username):
+def ssh_user_allow_ssh(auth, username):
     pass
 
 
-def ssh_user_disallow_ssh(username):
+def ssh_user_disallow_ssh(auth, username):
     pass
 
 
-def ssh_root_login_status():
+def ssh_root_login_status(auth):
     pass
 
 
-def ssh_root_login_enable():
+def ssh_root_login_enable(auth):
     pass
 
 
-def ssh_root_login_disable():
+def ssh_root_login_disable(auth):
     pass
 
 
-def ssh_key_list(username):
+def ssh_key_list(auth, username):
     pass
 
 
-def ssh_key_add(username, algo="default"):
+def ssh_key_add(auth, username, algo="default"):
     pass
 
 
-def ssh_key_import(username, public, private):
+def ssh_key_import(auth, username, public, private):
     pass
 
 
-def ssh_key_remove(username, key):
+def ssh_key_remove(auth, username, key):
     pass
 
 
-def ssh_authorized_keys_list(username):
+def ssh_authorized_keys_list(auth, username):
     pass
 
 
-def ssh_authorized_keys_add(username, key):
+def ssh_authorized_keys_add(auth, username, key):
     pass
 
 
-def ssh_authorized_keys_remove(username, key):
+def ssh_authorized_keys_remove(auth, username, key):
     pass
 
 
 # TODO
-# auth on critical commands
 # arguments in actionmap
