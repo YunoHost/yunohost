8ff9a303a6fbe9d0bb132ad571f5eb2c62fc43d3
Alexandre Aubin <alexandre.aubin@cern.ch>
2018-02-06 01:00:14 +0100

    Clumsy attempt to adapt the upgrade script to python

--- src/yunohost/data_migrations/0003_migrate_to_stretch.py
+++ src/yunohost/data_migrations/0003_migrate_to_stretch.py
@@ -5,11 +5,13 @@ import base64
 import time
 import json
 import errno
+import platform
 
 from moulinette import m18n
 from moulinette.core import MoulinetteError
 from moulinette.utils.log import getActionLogger
 from yunohost.tools import Migration
+from yunohost.service import _run_service_command, service_regen_conf
 
 logger = getActionLogger('yunohost.migration')
 
@@ -17,22 +19,62 @@ logger = getActionLogger('yunohost.migration')
 class MyMigration(Migration):
     "Upgrade the system to Debian Stretch and Yunohost 3.0"
 
+    mode = "manual"
+
     def backward(self):
-        # Not possible because that's a non-reversible operation ?
-        pass
+
+        raise MoulinetteError(m18n.n("migration_0003_backward_impossible"))
 
     def migrate(self):
 
+        self.logfile = "/tmp/{}.log".format(self.name)
+
         self.check_assertions()
 
-        pass
+        logger.warning(m18n.n("migration_0003_start", logfile=self.logfile))
 
+        # Preparing the upgrade
+        logger.warning(m18n.n("migration_0003_patching_sources_list"))
+        self.patch_apt_sources_list()
+        self.apt_update()
+        self.hold(["yunohost", "yunohost-admin", "moulinette", "ssowat", "fail2ban"])
+
+        # Main dist-upgrade
+        logger.warning(m18n.n("migration_0003_main_upgrade"))
+        _run_service_command("stop", "mysql")
+        self.apt_dist_upgrade(conf_flags=["old", "def"])
+        _run_service_command("start", "mysql")
+
+        # Specific upgrade for fail2ban...
+        logger.warning(m18n.n("migration_0003_fail2ban_upgrade"))
+        self.unhold(["fail2ban"])
+        os.system("mv /etc/fail2ban /etc/fail2ban.old")
+        self.apt_dist_upgrade(conf_flags=["new", "miss", "def"])
+        _run_service_command("restart", "mysql")
+
+        # Upgrade yunohost packages
+        logger.warning(m18n.n("migration_0003_yunohost_upgrade"))
+        self.unhold(["yunohost", "yunohost-admin", "moulinette", "ssowat"])
+        self.apt_install(["yunohost", "yunohost-admin", "moulinette", "ssowat"])
+
+        #yunohost service regen-conf fail2ban --debug --force
+        #yunohost service regen-conf --debug --force # We need this to refresh some conf files modified by upstream packages, e.g. mysql. But this will erase some legit user changes... Gotta think about some UX here :/
+        #
+        ## Clean the mess
+        #apt autoremove --assume-yes
+        #apt clean --assume-yes
 
     def check_assertions(self):
 
         # Be on jessie
+        debian_version = platform.dist()[1]
+        if not debian_version.startswith('8'):
+            raise MoulinetteError(m18n.n("migration_0003_not_jessie"))
 
         # Have > 1 Go free space on /var/ ?
+
+        # System up to date ?
+        # (e.g. with apt list --upgradable 2>&1 | grep -c upgradable)
 
         pass
 
@@ -46,3 +88,72 @@ class MyMigration(Migration):
         # Manually modified files ? (c.f. yunohost service regen-conf)
 
         return "Hurr durr itz dungerus"
+
+    def patch_apt_sources_list(self):
+
+        sources_list = glob.glob("/etc/apt/sources.list.d/*.conf")
+        sources_list.append("/etc/apt/sources.list")
+
+        # TODO / FIXME Is this enough ?
+        # (Probably not, sometimes there are some jessie/updates or
+        # jessie-updates ... but we don't want to touch to jessie-backports
+        # maybe ?)
+        # TODO/FIXME : to be seen if we really use 'vinaigrette' as final repo name
+        for f in sources_list:
+            command = "sed -i -e 's@ jessie @ stretch @g' " \
+                             "-e 's@repo.yunohost@vinaigrette\.yunohost@g' " \
+                             "{}".format(f)
+            print(command)
+            os.system(command)
+
+    def hold(self, packages):
+        for package in packages:
+            os.system("apt-mark hold {}".format(package))
+
+    def unhold(self, packages):
+        for package in packages:
+            os.system("apt-mark unhold {}".format(package))
+
+    def apt_update(self):
+
+        command = "apt-get update"
+        logger.debug("Running apt command :\n{}".format(command))
+        command += " 2>&1 | tee -a {}".format(self.logfile)
+
+        os.system(command)
+
+    def apt_install(self, packages):
+
+        command = ""
+        command += " DEBIAN_FRONTEND=noninteractive"
+        command += " APT_LISTCHANGES_FRONTEND=none"
+        command += " apt-get install"
+        command += " --assume-yes "
+        command += " ".join(packages)
+
+        logger.debug("Running apt command :\n{}".format(command))
+
+        command += " 2>&1 | tee -a {}".format(self.logfile)
+
+        os.system(command)
+
+    def apt_dist_upgrade(self, conf_flags):
+
+        # Make apt-get happy
+        os.system("echo 'libc6 libraries/restart-without-asking boolean true' | debconf-set-selections")
+
+        command = ""
+        command += " DEBIAN_FRONTEND=noninteractive"
+        command += " APT_LISTCHANGES_FRONTEND=none"
+        command += " apt-get"
+        command += " --fix-broken --show-upgraded --assume-yes"
+        for conf_flag in conf_flags:
+            command += ' -o Dpkg::Options::="--force-conf{}"'.format(conf_flag)
+        command += " dist-upgrade"
+
+        logger.debug("Running apt command :\n{}".format(command))
+
+        command += " 2>&1 | tee -a {}".format(self.logfile)
+
+        os.system(command)
+
