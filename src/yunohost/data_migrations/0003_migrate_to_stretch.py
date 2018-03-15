79d23728a226c6cc209548d88d4b5bac9404519d
Alexandre Aubin <alexandre.aubin@cern.ch>
2018-03-15 01:19:14 +0100

    Add a done message at the very end of the migration

--- src/yunohost/data_migrations/0003_migrate_to_stretch.py
+++ src/yunohost/data_migrations/0003_migrate_to_stretch.py
@@ -168,8 +168,8 @@ class MyMigration(Migration):
 
         wait_until_end_of_yunohost_command = "(while [ -f {} ]; do sleep 2; done)".format(MOULINETTE_LOCK)
 
-        command = "({} && {}) &".format(wait_until_end_of_yunohost_command,
-                                        upgrade_command)
+        command = "({} && {} && echo 'Done!') &".format(wait_until_end_of_yunohost_command,
+                                                        upgrade_command)
 
         logger.debug("Running command :\n{}".format(command))
 
