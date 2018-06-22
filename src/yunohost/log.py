f1181540c67938d2081dbca774a9c86e935f5ba2
Bram <cortex@worlddomination.be>
2018-06-22 05:34:18 +0200

    [mod] correct year for copyright stuff

--- src/yunohost/log.py
+++ src/yunohost/log.py
@@ -2,7 +2,7 @@
 
 """ License
 
-    Copyright (C) 2016 YunoHost
+    Copyright (C) 2018 YunoHost
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
