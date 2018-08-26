4e92a36322dae60021738c0d5ef71e67247a45a8
ljf <ljf+github@grimaud.me>
2018-08-26 20:20:35 +0200

    [fix] Backward if can't restart

--- src/yunohost/data_migrations/0006_manage_sshd_config.py
+++ src/yunohost/data_migrations/0006_manage_sshd_config.py
@@ -31,31 +31,36 @@ class MyMigration(Migration):
         # Manage SSHd in all case
         if os.path.exists('/etc/yunohost/from_script'):
             rm('/etc/yunohost/from_script')
-            copyfile('/etc/ssh/sshd_config', '/etc/ssh/sshd_config.restore')
+            copyfile('/etc/ssh/sshd_config', '/etc/ssh/sshd_config.bkp')
             service_regen_conf(names=['ssh'], force=True)
-            os.rename('/etc/ssh/sshd_config.restore', '/etc/ssh/sshd_config')
+            copyfile('/etc/ssh/sshd_config.bkp', '/etc/ssh/sshd_config')
 
         # If custom conf, add 'Include' instruction
         ynh_hash = _get_conf_hashes('ssh')['/etc/ssh/sshd_config']
         current_hash = _calculate_hash('/etc/ssh/sshd_config')
-        if ynh_hash == current_hash:
-            return
+        if ynh_hash != current_hash:
 
-        add_include = False
-        include_rgx = r'^[ \t]*Include[ \t]+sshd_config\.d/\*[ \t]*(?:#.*)?$'
-        for line in open('/etc/ssh/sshd_config'):
-            if re.match(include_rgx, line) is not None:
-                add_include = True
-                break
+            add_include = False
+            include_rgx = r'^[ \t]*Include[ \t]+sshd_config\.d/\*[ \t]*(?:#.*)?$'
+            for line in open('/etc/ssh/sshd_config'):
+                if re.match(include_rgx, line) is not None:
+                    add_include = True
+                    break
 
-        if add_include:
-            with open("/etc/ssh/sshd_config", "a") as conf:
-                conf.write('Include sshd_config.d/*')
+            if add_include:
+                with open("/etc/ssh/sshd_config", "a") as conf:
+                    conf.write('Include sshd_config.d/*')
 
+        # Restart ssh and backward if it fail
         if not _run_service_command('restart', 'ssh'):
             self.backward()
+            raise MoulinetteError(m18n.n("migration_0006_cancel"))
+
 
     def backward(self):
+        # We don't backward completely but it should be enough
 
-        raise MoulinetteError(m18n.n("migration_0006_backward_impossible"))
+        copyfile('/etc/ssh/sshd_config.bkp', '/etc/ssh/sshd_config')
+        if not _run_service_command('restart', 'ssh'):
+            raise MoulinetteError(m18n.n("migration_0006_cannot_restart"))
 
