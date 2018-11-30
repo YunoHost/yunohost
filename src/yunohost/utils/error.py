c4b77b2894e47847f59bf4bf058690b1cb6b24f4
Irina LAMBLA <irina@singularity.fr>
2018-12-01 00:12:16 +0100

    add YunohostError

---
+++ src/yunohost/utils/error.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+""" License
+
+    Copyright (C) 2018 YUNOHOST.ORG
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program; if not, see http://www.gnu.org/licenses
+
+"""
+
+from moulinette.core import MoulinetteError
+
+class YunohostError(MoulinetteError):
+    """Yunohost base exception"""
+    def __init__(self, key, *args, **kwargs):
+        msg = m18n.n(key, *args, **kwargs)
+        super(MoulinetteError, self).__init__(msg)
+
