f266cceb265559e8bf3d272b1b4837bb2206c08e
Josué Tille <josue@tille.ch>
2019-01-07 20:23:41 +0100

    Fix typo

--- src/yunohost/domain.py
+++ src/yunohost/domain.py
@@ -4,8 +4,7 @@
 
     Copyright (C) 2013 YunoHost
 
-    This progra
-    m is free software; you can redistribute it and/or modify
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
     by the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
