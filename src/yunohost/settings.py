a899102efc8a13fcf4c16bf31bb7769f4d820b23
Romuald du Song <romuald.dusong@deveryware.com>
2019-02-14 22:57:24 +0100

    don't share setting for nginx and ssh

--- src/yunohost/settings.py
+++ src/yunohost/settings.py
@@ -39,6 +39,7 @@ DEFAULTS = OrderedDict([
     ("security.password.admin.strength", {"type": "int", "default": 1}),
     ("security.password.user.strength", {"type": "int", "default": 1}),
     ("service.ssh.allow_deprecated_dsa_hostkey", {"type": "bool", "default": False}),
+    ("service.ssh.ciphers.compatibility", {"type": "enum", "choices": "intermediate", "modern"}),
     ("security.ciphers.compatibility", {"type": "enum", "choices": "intermediate", "modern"}),
 ])
 
