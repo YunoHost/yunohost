c4cd1359fb471835baff398f6ffe7b575bb30747
frju365 <abld@abld.info>
2019-02-18 14:51:19 +0100

    Strange migration

---
+++ src/yunohost/data_migrations/0009_dh_params.py
@@ -0,0 +1,37 @@
+import re
+import os
+
+from moulinette import m18n
+from moulinette.utils.log import getActionLogger
+from moulinette.utils.filesystem import chown
+
+from yunohost.tools import Migration
+from yunohost.service import service_regen_conf
+from yunohost.settings import settings_set, settings_get
+
+command = "nice -n 19 openssl dhparam -out /etc/ssl/private/dh2048.pem -outform PEM -2 2048 -dsaparam 2> /var/log/yunohost/dhparam_generation.log && chown root:ssl-cert /etc/ssl/private/dh2048.pem && rm /etc/cron.hourly/yunohost-generate-dh-params\n"
+dhparams_file = "/etc/ssl/private/dh2048.pem"
+
+class MyMigration(Migration):
+    "This migration will add dh_params line and generate it in installed instance"
+
+    def migrate(self):
+
+        try:
+            file_open = open(dhparams_file)
+            with open(cron_job_file, "w") as f:
+                        f.write("#!/bin/bash\n")
+                        f.write(command)
+
+                    _set_permissions(cron_job_file, "root", "root", 0o755)
+                    service_regen_conf(['nginx'], force=True)
+        except:
+            service_regen_conf(['nginx'], force=True)
+
+
+    def backward(self):
+        try:
+            file_open = open(dhparams_file)
+            os.remove(dhparams_file)
+        except:
+            pass
