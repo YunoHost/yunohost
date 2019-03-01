12c49074e35fb569e623fbf63aae2e57288890a1
frju365 <abld@abld.info>
2019-03-01 12:58:18 +0100

    fix variable : cron_job_file

--- src/yunohost/data_migrations/0009_dh_params.py
+++ src/yunohost/data_migrations/0009_dh_params.py
@@ -7,6 +7,7 @@ from moulinette.utils.filesystem import chown
 from yunohost.tools import Migration
 from yunohost.service import service_regen_conf
 
+cron_job_file = "/etc/cron.hourly/yunohost-generate-dh-params"
 command = "nice -n 19 openssl dhparam -out /etc/ssl/private/dh2048.pem -outform PEM -2 2048 -dsaparam 2> /var/log/yunohost/dhparam_generation.log && chown root:ssl-cert /etc/ssl/private/dh2048.pem && yunohost service regen-conf >> /var/log/yunohost/dhparam_generation.log && rm /etc/cron.hourly/yunohost-generate-dh-params\n"
 dhparams_file = "/etc/ssl/private/dh2048.pem"
 
