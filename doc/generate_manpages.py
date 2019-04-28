1fcffed58db2a8bf5127194c2fa258cce56cf065
Laurent Peuch <cortex@worlddomination.be>
2019-04-28 03:52:38 +0200

    [mod] blank lines for lisibility

--- doc/generate_manpages.py
+++ doc/generate_manpages.py
@@ -30,14 +30,19 @@ with open(ACTIONSMAP_FILE, 'r') as stream:
 
     # Getting the dictionary containning what actions are possible per domain
     OPTION_TREE = yaml.safe_load(stream)
+
     DOMAINS = [x for x in OPTION_TREE.keys() if not x.startswith('_')]
     DOMAINS_STR = '"{}"'.format(' '.join(DOMAINS))
+
     ACTIONS_DICT = {}
+
     for domain in DOMAINS:
         ACTIONS = [str for str in OPTION_TREE[domain]['actions'].keys()
                    if not str.startswith('_')]
+
         ACTIONS_STR = '"{}"'.format(' '.join(ACTIONS))
         ACTIONS_DICT[domain] = ACTIONS_STR
+
         for action in ACTIONS:
             # print("yunohost", domain, action)
             cmd = "sudo help2man \" yunohost " + domain + "  " + action + " --help \" -o " + OUTPUT_DIR + "yunohost_" + domain + "_" + action
