fae7a99f27c58a44c8637d7af900d5c405848e6b
frju365 <abld@abld.info>
2019-09-30 17:08:57 +0200

    Function cron add

--- src/yunohost/data_migrations/0013_dh_params.py
+++ src/yunohost/data_migrations/0013_dh_params.py
@@ -6,6 +6,7 @@ from moulinette.utils.filesystem import chown
 
 from yunohost.tools import Migration
 from yunohost.certificate import _set_permissions
+from yunohost.tools import cron_add
 
 cron_job_file = "/etc/cron.hourly/yunohost-generate-dh-params"
 command = "nice -n 19 openssl dhparam -out /etc/ssl/private/dh2048.pem -outform PEM -2 2048 -dsaparam 2> /var/log/yunohost/dhparam_generation.log && chown root:ssl-cert /etc/ssl/private/dh2048.pem && yunohost service regen-conf >> /var/log/yunohost/dhparam_generation.log && rm /etc/cron.hourly/yunohost-generate-dh-params\n"
@@ -16,13 +17,8 @@ class MyMigration(Migration):
     "Add dh_params line and generate it in installed instance"
 
     def run(self):
-
         if not os.path.exists(dhparams_file):
-            with open(cron_job_file, "w") as f:
-                        f.write("#!/bin/bash\nset -eux\n")
-                        f.write(command)
-
-            _set_permissions(cron_job_file, "root", "root", 0o755)
+            cron_add(command, cron_job_file)
 
 
     def backward(self):
