66c5ad18c9f66ba0fcea99683d69de98a31a86b0
Alexandre Aubin <alex.aubin@mailoo.org>
2019-11-08 23:17:32 +0100

    Migration 12 -> 13

--- src/yunohost/data_migrations/0010_migrate_to_apps_json.py
+++ src/yunohost/data_migrations/0010_migrate_to_apps_json.py
@@ -6,8 +6,8 @@ logger = getActionLogger('yunohost.migration')
 
 class MyMigration(Migration):
 
-    "Migrate from official.json to apps.json (outdated, replaced by migration 12)"
+    "Migrate from official.json to apps.json (outdated, replaced by migration 13)"
 
     def run(self):
-        logger.info("This migration is oudated and doesn't do anything anymore. The migration 12 will handle this instead.")
+        logger.info("This migration is oudated and doesn't do anything anymore. The migration 13 will handle this instead.")
         pass
