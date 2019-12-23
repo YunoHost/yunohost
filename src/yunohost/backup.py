ca4c4c2e621bf64bf511def64637c6387fd4309f
Josué Tille <josue@tille.ch>
2019-12-23 16:10:13 +0100

    Remove unused import

--- src/yunohost/backup.py
+++ src/yunohost/backup.py
@@ -1188,7 +1188,7 @@ class RestoreManager():
             return
 
         from yunohost.user import user_group_list
-        from yunohost.permission import permission_create, permission_delete, user_permission_update, user_permission_list, permission_sync_to_user
+        from yunohost.permission import permission_create, permission_delete, user_permission_list, permission_sync_to_user
 
         # Backup old permission for apps
         # We need to do that because in case of an app is installed we can't remove the permission for this app
@@ -1292,7 +1292,7 @@ class RestoreManager():
         restore_app_failed -- Raised if the restore bash script failed
         """
         from yunohost.user import user_group_list
-        from yunohost.permission import permission_create, permission_delete, user_permission_list, user_permission_update, permission_sync_to_user
+        from yunohost.permission import permission_create, permission_delete, user_permission_list, permission_sync_to_user
 
         def copytree(src, dst, symlinks=False, ignore=None):
             for item in os.listdir(src):
