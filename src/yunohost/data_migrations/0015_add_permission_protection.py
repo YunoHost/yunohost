97c3a0fb9b2763cf297ea691c78d44f7c503b9ce
Josué Tille <josue@tille.ch>
2019-12-28 22:29:52 +0100

    Fix migration

--- src/yunohost/data_migrations/0015_add_permission_protection.py
+++ src/yunohost/data_migrations/0015_add_permission_protection.py
@@ -24,14 +24,24 @@ class MyMigration(Migration):
     def run(self):
 
         from yunohost.utils.ldap import _get_ldap_interface
+        from yunohost.regenconf import regen_conf, BACKUP_CONF_DIR
+
+        # Check if the migration can be processed
+        ldap_regen_conf_status = regen_conf(names=['slapd'], dry_run=True)
+        # By this we check if the have been customized
+        if ldap_regen_conf_status and ldap_regen_conf_status['slapd']['pending']:
+            logger.warning(m18n.n("migration_0011_slapd_config_will_be_overwritten", conf_backup_folder=BACKUP_CONF_DIR))
+
+        regen_conf(names=['slapd'], force=True)
+
         ldap = _get_ldap_interface()
 
-        permission_list = user_permission_list(short=True)
-        
+        permission_list = user_permission_list(short=True)["permissions"]
+
         for permission in permission_list:
-            if permission in SYSTEM_PERMS:
-                ldap.update('cn=%s,ou=permission' % permission, 'isProtected': "TRUE"})
-            elif permission.end_with(".main"):
-                ldap.update('cn=%s,ou=permission' % permission, 'isProtected': "FALSE"})
+            if permission.split('.')[0] in SYSTEM_PERMS:
+                ldap.update('cn=%s,ou=permission' % permission, {'isProtected': "TRUE"})
+            elif permission.endswith(".main"):
+                ldap.update('cn=%s,ou=permission' % permission, {'isProtected': "FALSE"})
             else:
-                ldap.update('cn=%s,ou=permission' % permission, 'isProtected': "TRUE"})
+                ldap.update('cn=%s,ou=permission' % permission, {'isProtected': "TRUE"})
