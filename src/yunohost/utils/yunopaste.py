4df46fec68144dad3934c44604140aa036d19dda
Alexandre Aubin <alexandre.aubin@cern.ch>
2020-03-21 23:08:24 +0100

    Rename http diagnoser to web, should feel slightly less technical

--- src/yunohost/utils/yunopaste.py
+++ src/yunohost/utils/yunopaste.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 from yunohost.utils.error import YunohostError
-
+from yunohost.domain import domain_list
 
 def yunopaste(data):
 
@@ -24,3 +24,8 @@ def yunopaste(data):
         raise YunohostError("Uhoh, couldn't parse the answer from paste.yunohost.org : %s" % r.text, raw_msg=True)
 
     return "%s/raw/%s" % (paste_server, url)
+
+
+def anonymize(data):
+
+    pass
