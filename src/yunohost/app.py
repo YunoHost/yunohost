b9aa5d143f7b179373f8bc36ec3b7d147d1bb083
Kay0u <pierre.moltess@gmail.com>
2020-03-22 20:51:32 +0100

    Allow public apps with no sso tile

--- src/yunohost/app.py
+++ src/yunohost/app.py
@@ -437,7 +437,7 @@ def app_map(app=None, raw=False, user=None):
                 logger.warning("Uhoh, no main permission was found for app %s ... sounds like an app was only partially removed due to another bug :/" % app_id)
                 continue
             main_perm = permissions[app_id + ".main"]
-            if user not in main_perm["corresponding_users"] and "visitors" not in main_perm["allowed"]:
+            if user not in main_perm["corresponding_users"]:
                 continue
 
         domain = app_settings['domain']
