97ab8c91f86c43dcec728253a2e3bd6a8304208c
Alexandre Aubin <alex.aubin@mailoo.org>
2020-04-19 02:03:35 +0200

    Fix the fix for stupid 'search' stuff in resolvconf ... + let's in fact ignore it if it does exists in /etc/resolv.conf >.>

--- data/hooks/diagnosis/10-ip.py
+++ data/hooks/diagnosis/10-ip.py
@@ -134,7 +134,7 @@ class IPDiagnoser(Diagnoser):
     def good_resolvconf(self):
         content = read_file("/etc/resolv.conf").strip().split("\n")
         # Ignore comments and empty lines
-        content = [l.strip() for l in content if l.strip() and not l.strip().startswith("#")]
+        content = [l.strip() for l in content if l.strip() and not l.strip().startswith("#") and not l.strip().startswith("search")]
         # We should only find a "nameserver 127.0.0.1"
         return len(content) == 1 and content[0].split() == ["nameserver", "127.0.0.1"]
 
