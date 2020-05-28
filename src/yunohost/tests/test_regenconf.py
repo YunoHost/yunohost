28a922de51caec3c6ea44848eb928f404d4abd02
Alexandre Aubin <alex.aubin@mailoo.org>
2020-05-28 21:37:15 +0200

    Get rid of old hashes of file that 'are expected to be removed' and are indeed 'already removed'

--- src/yunohost/tests/test_regenconf.py
+++ src/yunohost/tests/test_regenconf.py
@@ -1,8 +1,4 @@
-import glob
 import os
-import pytest
-import shutil
-import requests
 
 from conftest import message, raiseYunohostError
 
@@ -14,7 +10,8 @@ from yunohost.utils.error import YunohostError
 from yunohost.regenconf import manually_modified_files, regen_conf, _get_conf_hashes, _force_clear_hashes
 
 TEST_DOMAIN = "secondarydomain.test"
-TEST_DOMAIN_NGINX_CONFIG = "/etc/nginx/conf.d/secondarydomain.test.conf"
+TEST_DOMAIN_NGINX_CONFIG = "/etc/nginx/conf.d/%s.conf" % TEST_DOMAIN
+TEST_DOMAIN_DNSMASQ_CONFIG = "/etc/dnsmasq.d/%s" % TEST_DOMAIN
 SSHD_CONFIG = "/etc/ssh/sshd_config"
 
 def setup_function(function):
@@ -22,10 +19,12 @@ def setup_function(function):
     _force_clear_hashes([TEST_DOMAIN_NGINX_CONFIG])
     clean()
 
+
 def teardown_function(function):
 
     clean()
     _force_clear_hashes([TEST_DOMAIN_NGINX_CONFIG])
+
 
 def clean():
 
@@ -116,3 +115,49 @@ def test_ssh_conf_unmanaged_and_manually_modified(mocker):
 
     assert SSHD_CONFIG in _get_conf_hashes("ssh")
     assert SSHD_CONFIG not in manually_modified_files()
+
+
+def test_stale_hashes_get_removed_if_empty():
+    """
+    This is intended to test that if a file gets removed and is indeed removed,
+    we don't keep a useless empty hash corresponding to an old file.
+    In this case, we test this using the dnsmasq conf file (we don't do this
+    using the nginx conf file because it's already force-removed during
+    domain_remove())
+    """
+
+    domain_add(TEST_DOMAIN)
+
+    assert os.path.exists(TEST_DOMAIN_DNSMASQ_CONFIG)
+    assert TEST_DOMAIN_DNSMASQ_CONFIG in _get_conf_hashes("dnsmasq")
+
+    domain_remove(TEST_DOMAIN)
+
+    assert not os.path.exists(TEST_DOMAIN_DNSMASQ_CONFIG)
+    assert TEST_DOMAIN_DNSMASQ_CONFIG not in _get_conf_hashes("dnsmasq")
+
+
+def test_stale_hashes_if_file_manually_deleted():
+    """
+    Same as other test, but manually delete the file in between and check
+    behavior
+    """
+
+    domain_add(TEST_DOMAIN)
+
+    assert os.path.exists(TEST_DOMAIN_DNSMASQ_CONFIG)
+    assert TEST_DOMAIN_DNSMASQ_CONFIG in _get_conf_hashes("dnsmasq")
+
+    os.remove(TEST_DOMAIN_DNSMASQ_CONFIG)
+
+    assert not os.path.exists(TEST_DOMAIN_DNSMASQ_CONFIG)
+
+    regen_conf(names=["dnsmasq"])
+
+    assert not os.path.exists(TEST_DOMAIN_DNSMASQ_CONFIG)
+    assert TEST_DOMAIN_DNSMASQ_CONFIG in _get_conf_hashes("dnsmasq")
+
+    domain_remove(TEST_DOMAIN)
+
+    assert not os.path.exists(TEST_DOMAIN_DNSMASQ_CONFIG)
+    assert TEST_DOMAIN_DNSMASQ_CONFIG not in _get_conf_hashes("dnsmasq")
