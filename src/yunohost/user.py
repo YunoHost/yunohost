fc26837aa789fd228fe863e354de9caa249a83e9
Alexandre Aubin <alex.aubin@mailoo.org>
2021-04-10 01:04:59 +0200

    security: Enforce permissions on /home/ so that they can't sneak in each other home

--- src/yunohost/user.py
+++ src/yunohost/user.py
@@ -229,6 +229,11 @@ def user_create(
         if not os.path.isdir("/home/{0}".format(username)):
             logger.warning(m18n.n("user_home_creation_failed"), exc_info=1)
 
+    try:
+        subprocess.check_call(["setfacl", "-m", "g:all_users:---", "/home/%s" % username])
+    except subprocess.CalledProcessError:
+        logger.warning("Failed to protect /home/%s" % username, exc_info=1)
+
     # Create group for user and add to group 'all_users'
     user_group_create(groupname=username, gid=uid, primary_group=True, sync_perm=False)
     user_group_update(groupname="all_users", add=username, force=True, sync_perm=True)
