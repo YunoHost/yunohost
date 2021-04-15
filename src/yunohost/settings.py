37c0825eed421acadd0a7528dbab8172695eed8f
Alexandre Aubin <alex.aubin@mailoo.org>
2021-04-15 12:22:21 +0200

    Also propagate ssh port on fail2ban config

--- src/yunohost/settings.py
+++ src/yunohost/settings.py
@@ -387,11 +387,16 @@ def reconfigure_nginx(setting_name, old_value, new_value):
         regen_conf(names=["nginx"])
 
 
-@post_change_hook("security.ssh.port")
 @post_change_hook("security.ssh.compatibility")
 def reconfigure_ssh(setting_name, old_value, new_value):
     if old_value != new_value:
         regen_conf(names=["ssh"])
+
+
+@post_change_hook("security.ssh.port")
+def reconfigure_ssh_and_fail2ban(setting_name, old_value, new_value):
+    if old_value != new_value:
+        regen_conf(names=["ssh", "fail2ban"])
 
 
 @post_change_hook("smtp.allow_ipv6")
