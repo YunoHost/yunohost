46fb84e071a65699d7685896d6acdbee27e6e585
Alexandre Aubin <alex.aubin@mailoo.org>
2021-09-02 04:46:31 +0200

    Expand the reformat locale script to also cover en.json ...

--- tests/reformat_locales.py
+++ tests/reformat_locales.py
@@ -1,11 +1,26 @@
 import re
 
-locale_folder = "locales/"
-locale = open(locale_folder + "fr.json").read()
+def reformat(lang, transformations):
+
+    locale = open(f"locales/{lang}.json").read()
+    for pattern, replace in transformations.items():
+        locale = re.compile(pattern).sub(replace, locale)
+
+    open(f"locales/{lang}.json", "w").write(locale)
+
+######################################################
 
 godamn_spaces_of_hell = ["\u00a0", "\u2000", "\u2001", "\u2002", "\u2003", "\u2004", "\u2005", "\u2006", "\u2007", "\u2008", "\u2009", "\u200A", "\u202f", "\u202F", "\u3000"]
 
 transformations = {s: " " for s in godamn_spaces_of_hell}
+transformations.update({
+    "…": "...",
+})
+
+
+reformat("en", transformations)
+
+######################################################
 
 transformations.update({
     "courriel": "email",
@@ -16,12 +31,8 @@ transformations.update({
     "«": "'",
     " »": "'",
     "»": "'",
-    "…": "...",
     "’": "'",
     #r"$(\w{1,2})'|( \w{1,2})'": r"\1\2’",
 })
 
-for pattern, replace in transformations.items():
-    locale = re.compile(pattern).sub(replace, locale)
-
-open(locale_folder + "fr.json", "w").write(locale)
+reformat("fr", transformations)
