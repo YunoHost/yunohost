4f010864b54450204b90b2d7141a663af6398aa6
ljf (zamentur) <zamentur@users.noreply.github.com>
2022-03-31 14:48:11 +0200

    [fix] Bad set operation

--- src/migrations/0021_migrate_to_bullseye.py
+++ src/migrations/0021_migrate_to_bullseye.py
@@ -301,7 +301,9 @@ class MyMigration(Migration):
         # which means maybe a previous upgrade crashed and we're re-running it)
         if " bullseye " not in read_file("/etc/apt/sources.list"):
             tools_update(target="system")
-            upgradable_system_packages = set(list(_list_upgradable_apt_packages()))
+            upgradable_system_packages = list(_list_upgradable_apt_packages())
+            upgradable_system_packages = [package["name"] for package in upgradable_system_packages]
+            upgradable_system_packages = set(upgradable_system_packages)
             # Lime2 have hold packages to avoid ethernet instability
             # See https://github.com/YunoHost/arm-images/commit/b4ef8c99554fd1a122a306db7abacc4e2f2942df
             lime2_hold_packages = set([
