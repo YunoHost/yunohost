95d10c3b92ce25b68e0a788ed4262ce413a322c0
Tagadda <36127788+Tagadda@users.noreply.github.com>
2022-04-26 20:57:28 +0000

    helpers: new apt test for build deps

--- tests/test_helpers.d/ynhtest_apt.sh
+++ tests/test_helpers.d/ynhtest_apt.sh
@@ -1,18 +1,25 @@
 ynhtest_apt_install_apt_deps_regular() {
 
     dpkg --list | grep -q "ii *$app-ynh-deps" && apt remove $app-ynh-deps --assume-yes || true
+    dpkg --list | grep -q "ii *$app-ynh-build-deps" && apt remove $app-ynh-deps --assume-yes || true
     dpkg --list | grep -q 'ii *nyancat' && apt remove nyancat --assume-yes || true
     dpkg --list | grep -q 'ii *sl' && apt remove sl --assume-yes || true
 
     ! ynh_package_is_installed "$app-ynh-deps"
+    ! ynh_package_is_installed "$app-ynh-build-deps"
     ! ynh_package_is_installed "nyancat"
     ! ynh_package_is_installed "sl"
 
     ynh_install_app_dependencies "nyancat sl"
 
     ynh_package_is_installed "$app-ynh-deps"
+    ynh_package_is_installed "$app-ynh-build-deps"
     ynh_package_is_installed "nyancat"
     ynh_package_is_installed "sl"
+
+    ynh_remove_app_build_dependencies
+
+    ! ynh_package_is_installed "$app-ynh-build-deps"
     
     ynh_remove_app_dependencies
 
