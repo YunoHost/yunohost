d05f192a4f4d5b511577213302e541d7c62fb0dc
tituspijean <tituspijean@outlook.com>
2022-04-28 21:42:19 +0200

    Update n checksum with autoupdater

--- .github/workflows/n_updater.sh
+++ .github/workflows/n_updater.sh
@@ -71,7 +71,7 @@ echo "Calculated checksum for n v${version} is $checksum"
 sed -i -E "s/^n_version=.*$/n_version=$version/" helpers/nodejs
 
 # Replace checksum in helper
-sed -i -E "s/^n_checksum=.*$/n_checksum=$checksum\"/" helpers/nodejs
+sed -i -E "s/^n_checksum=.*$/n_checksum=$checksum/" helpers/nodejs
 
 # The Action will proceed only if the PROCEED environment variable is set to true
 echo "PROCEED=true" >> $GITHUB_ENV
