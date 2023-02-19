50f86af51a10b25a6c31837b37daf7023c9279ec
Alexandre Aubin <alex.aubin@mailoo.org>
2023-02-19 19:40:28 +0100

    quality: unused function

--- src/tools.py
+++ src/tools.py
@@ -474,13 +474,6 @@ def tools_upgrade(operation_logger, target=None):
 
         logger.debug("Running apt command :\n{}".format(dist_upgrade))
 
-        def is_relevant(line):
-            irrelevants = [
-                "service sudo-ldap already provided",
-                "Reading database ...",
-            ]
-            return all(i not in line.rstrip() for i in irrelevants)
-
         callbacks = (
             lambda l: logger.info("+ " + l.rstrip() + "\r")
             if _apt_log_line_is_relevant(l)
