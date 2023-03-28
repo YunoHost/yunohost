44be316673452012cf6c27b7b95025863a0f2916
André Théo LAURET <118362885+eldertek@users.noreply.github.com>
2023-03-28 19:22:19 +0400

    fix issue 2177

--- src/utils/system.py
+++ src/utils/system.py
@@ -89,15 +89,15 @@ def binary_to_human(n: int) -> str:
     """
     Convert bytes or bits into human readable format with binary prefix
     """
-    symbols = ("K", "M", "G", "T", "P", "E", "Z", "Y")
+    symbols = ("M", "G", "T", "P", "E", "Z", "Y")
     prefix = {}
     for i, s in enumerate(symbols):
         prefix[s] = 1 << (i + 1) * 10
     for s in reversed(symbols):
         if n >= prefix[s]:
             value = float(n) / prefix[s]
-            return "%.1f%s" % (value, s)
-    return "%s" % n
+            return "%d%s" % (value, s)
+    return "%sK" % n
 
 
 def ram_available():
