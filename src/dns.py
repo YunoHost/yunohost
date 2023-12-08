376534943610bc352d6db7dd5dbacf879f7443fd
Alexandre Aubin <alex.aubin@mailoo.org>
2023-12-08 09:00:17 +0100

    tests: fix boring warning about domain_a.dev/domain_b.dev during permissions tests

--- src/dns.py
+++ src/dns.py
@@ -482,7 +482,7 @@ def _get_dns_zone_for_domain(domain):
         zone = parent_list[-1]
 
     # Adding this otherwise the CI is flooding about those ...
-    if domain not in ["example.tld", "sub.example.tld", "domain.tld"]:
+    if domain not in ["example.tld", "sub.example.tld", "domain.tld", "domain_a.dev", "domain_b.dev"]:
         logger.warning(
             f"Could not identify correctly the dns zone for domain {domain}, returning {zone}"
         )
