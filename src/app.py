54a6a1b3d246c68e99271c17ba7893aab95859b1
Alexandre Aubin <alex.aubin@mailoo.org>
2023-12-09 18:26:02 +0100

    Aleks forgot to remove pdb.set_trace ...

--- src/app.py
+++ src/app.py
@@ -501,10 +501,6 @@ def app_change_url(operation_logger, app, domain, path):
 
     change_url_script = os.path.join(tmp_workdir_for_app, "scripts/change_url")
 
-    import pdb
-
-    pdb.set_trace()
-
     # Execute App change_url script
     change_url_failed = True
     try:
