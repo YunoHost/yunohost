ff6b6954aafbbb91d83110ef408f4164b60ed54f
tituspijean <8769166+tituspijean@users.noreply.github.com>
2024-04-10 20:32:30 +0000

    :art: Format Python code with Black

--- src/utils/resources.py
+++ src/utils/resources.py
@@ -152,7 +152,11 @@ class AppResource:
             "__APP__": self.app,
             "__YNH_ARCH__": system_arch(),
             "__YNH_DEBIAN_VERSION__": debian_version(),
-            "__YNH_APP_UPSTREAM_VERSION__": manager.wanted["version"].split("~")[0] if manager.wanted else manager.current["version"].split("~")[0],
+            "__YNH_APP_UPSTREAM_VERSION__": (
+                manager.wanted["version"].split("~")[0]
+                if manager.wanted
+                else manager.current["version"].split("~")[0]
+            ),
         }
 
         def recursive_apply(function: Callable, data: Any) -> Any:
