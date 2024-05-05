878b6d574d0162fb658cbbb70cb0c39c62aba62b
tituspijean <titus+yunohost@pijean.ovh>
2024-05-05 18:51:50 +0200

    Split log rm commands into separate shells
    otherwise, if one fails, the following ones are not run.

--- src/tools.py
+++ src/tools.py
@@ -641,10 +641,10 @@ def tools_basic_space_cleanup():
     """
     subprocess.run("apt autoremove && apt autoclean", shell=True)
     subprocess.run("journalctl --vacuum-size=50M", shell=True)
-    subprocess.run(
-        "rm /var/log/*.gz && rm /var/log/*/*.gz && rm /var/log/*.? && rm /var/log/*/*.?",
-        shell=True,
-    )
+    subprocess.run("rm /var/log/*.gz", shell=True)
+    subprocess.run("rm /var/log/*/*.gz", shell=True)
+    subprocess.run("rm /var/log/*.?", shell=True)
+    subprocess.run("rm /var/log/*/*.?", shell=True)
 
 
 # ############################################ #
