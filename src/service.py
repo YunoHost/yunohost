131760e30c8221ded5187af8ec440eeaaa503791
OniriCorpe <oniricorpe@disroot.org>
2024-07-01 22:04:28 +0200

    trying to fix #1886

--- src/service.py
+++ src/service.py
@@ -26,7 +26,7 @@ from glob import glob
 from datetime import datetime
 
 from moulinette import m18n
-from yunohost.diagnosis import diagnosis_ignore, diagnosis_unignore
+from yunohost.diagnosis import _diagnosis_ignore
 from yunohost.utils.error import YunohostError, YunohostValidationError
 from moulinette.utils.process import check_output
 from moulinette.utils.log import getActionLogger
@@ -297,9 +297,7 @@ def service_enable(names):
         names = [names]
     for name in names:
         if _run_service_command("enable", name):
-            services = _get_services()
-            if name in services:
-                diagnosis_unignore({"services": [{"service": name}]})
+            _diagnosis_ignore(remove_filter="services", list=f"service={name}")
             logger.success(m18n.n("service_enabled", service=name))
         else:
             raise YunohostError(
@@ -319,9 +317,7 @@ def service_disable(names):
         names = [names]
     for name in names:
         if _run_service_command("disable", name):
-            services = _get_services()
-            if name in services:
-                diagnosis_ignore({"services": [{"service": name}]})
+            _diagnosis_ignore(add_filter="services", list=f"service={name}")
             logger.success(m18n.n("service_disabled", service=name))
         else:
             raise YunohostError(
