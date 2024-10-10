1659147b9f68c477bac17beceb96a31524798266
Thatoo <Thatoo@users.noreply.github.com>
2024-10-10 16:20:43 +0200

    Update resources.py

--- src/utils/resources.py
+++ src/utils/resources.py
@@ -355,9 +355,9 @@ class SourcesResource(AppResource):
         armhf.sha256 = "4355a46b19d348dc2f57c046f8ef63d4538ebb936000f3c9ee954a27460dd865"
 
         autoupdate.strategy = "latest_github_release"
-        autoupdate.asset.amd64 = ".*\\.amd64.tar.gz"
-        autoupdate.asset.i386 = ".*\\.386.tar.gz"
-        autoupdate.asset.armhf = ".*\\.arm.tar.gz"
+        autoupdate.asset.amd64 = ".*\\\\.amd64.tar.gz"
+        autoupdate.asset.i386 = ".*\\\\.386.tar.gz"
+        autoupdate.asset.armhf = ".*\\\\.arm.tar.gz"
 
         [resources.sources.zblerg]
         url = "https://zblerg.com/download/zblerg"
