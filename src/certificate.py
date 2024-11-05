ed5b39b90e19c18989e497345d264829c719d220
alexAubin <alexAubin@users.noreply.github.com>
2024-11-05 13:46:32 +0000

    :art: Format Python code with Black, and Bash code with Shfmt

--- src/certificate.py
+++ src/certificate.py
@@ -600,15 +600,13 @@ def _prepare_certificate_signing_request(domain, key_file, output_folder):
         subsanlist = [f"DNS:{sub}.{domain}" for sub in sanlist if "." not in sub]
         # This is meant for situation such as cryptpad where we need to be able to have a cert for sandbox-domain.tld (with a dash, not just sandbox.domain.tld)
         domainsanlist = [f"DNS:{domain}" for domain in sanlist if "." in domain]
-        sanlist = ", ".join(subsanlist+domainsanlist)
+        sanlist = ", ".join(subsanlist + domainsanlist)
         csr.add_extensions(
             [
                 crypto.X509Extension(
                     b"subjectAltName",
                     False,
-                    sanlist.encode(
-                        "utf-8"
-                    ),
+                    sanlist.encode("utf-8"),
                 )
             ]
         )
