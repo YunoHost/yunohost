f624036b2b933fc6def9c08c77cbbe98921f080a
axolotle <nchesnais@mailoo.org>
2024-11-28 12:02:27 +0100

    refactor(AppOption): update with `get_annotation()`

--- src/utils/form.py
+++ src/utils/form.py
@@ -1570,7 +1570,7 @@ class DomainOption(BaseSelectOption):
         return value
 
 
-class AppOption(BaseChoicesOption):
+class AppOption(BaseSelectOption):
     """
     Ask for a user app.
     Renders as a select in the web-admin and as a regular prompt in CLI with autocompletion of installed apps.
@@ -1592,7 +1592,7 @@ class AppOption(BaseChoicesOption):
 
     type: Literal[OptionType.app] = OptionType.app
     filter: JSExpression | None = None
-    choices: dict[str, str] | None = None
+    choices: dict[str, str]
 
     @model_validator(mode="before")
     @classmethod
