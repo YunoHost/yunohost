b5afc8ccbe6bc335ccc325168ffc1d04225a5079
Christophe Henry <contact@c-henry.fr>
2025-02-02 15:55:36 +0100

    Apply suggestions from peer review

--- src/storage.py
+++ src/storage.py
@@ -1,9 +1,10 @@
-def storage_disk_list(name, human_readable=False):
-    from yunohost.disks import info
-
-    return info(name, human_readable=human_readable)
-
-def storage_disk_info(with_info=False, human_readable=False):
+def storage_disk_list(with_info=False, human_readable=False):
     from yunohost.disks import list
 
     return list(with_info=with_info, human_readable=human_readable)
+
+
+def storage_disk_info(name, human_readable=False):
+    from yunohost.disks import info
+
+    return info(name, human_readable=human_readable)
