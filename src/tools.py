9d56b36f327ceeb4e0814a5028e4454aa6758a15
gabjy <gabjy@tuta.com>
2025-09-01 08:53:33 +0000

    Update src/tools.py
    Co-authored-by: Alexandre Aubin <4533074+alexAubin@users.noreply.github.com>

--- src/tools.py
+++ src/tools.py
@@ -717,7 +717,7 @@ def tools_clean_old_kernels() -> None:
     # Removing kernel except last 2
     uname_output = subprocess.run("uname -r", capture_output=True, text=True, shell=True)
     running_kernel = uname_output.stdout.split("\n")[:-1]
-    dpkg_output = subprocess.run("dpkg -l | grep linux-image | awk '{print$2}'", capture_output=True, text=True, shell=True)
+    dpkg_output = subprocess.run("dpkg -l | grep '^ii\s*linux-image' | awk '{print $2}'", capture_output=True, text=True, shell=True)
     if not dpkg_output.stdout.strip():
         logger.info("Couldn't find installed kernels. Maybe you are running Yunohost in an LXC container ?")
         return
