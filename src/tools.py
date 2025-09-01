d74366deb851c1f768cbc0ed1db37dd39c43532f
gabjy <gabjy@tuta.com>
2025-09-01 12:22:35 +0200

    implementing suggestions + cleaing up the conditions

--- src/tools.py
+++ src/tools.py
@@ -716,22 +716,18 @@ def tools_basic_space_cleanup() -> None:
 def tools_clean_old_kernels() -> None:
     # Removing kernel except last 2
     uname_output = subprocess.run("uname -r", capture_output=True, text=True, shell=True)
-    running_kernel = uname_output.stdout.split("\n")[:-1]
+    running_kernel = uname_output.stdout.strip().split("\n")
     dpkg_output = subprocess.run("dpkg -l | grep '^ii\s*linux-image' | awk '{print $2}'", capture_output=True, text=True, shell=True)
-    if not dpkg_output.stdout.strip():
-        logger.info("Couldn't find installed kernels. Maybe you are running Yunohost in an LXC container ?")
+    kernels = dpkg_output.stdout.strip().split("\n")[:-3]
+    if not kernels:
+        logger.info("No kernel to remove")
         return
-    else:
-        kernels = dpkg_output.stdout.split("\n")[:-4]
-        if kernels == []:
-            logger.info("No kernel to remove")
-            return
-        else:    
-            for kernel in kernels:
-                if running_kernel[0] not in kernel:
-                    subprocess.run("apt remove -y --purge " + kernel, shell=True)
-                else:
-                    print("This is the running kernel, it won't be removed")
+    else:    
+        for kernel in kernels:
+            if running_kernel[0] not in kernel:
+                subprocess.run("apt remove -y --purge " + kernel, shell=True)
+            else:
+                print("This is the running kernel, it won't be removed")
 
 
 # ############################################ #
