c427bdc8f572001e5dcb18bf49955afe6b7d2a39
Josué Tille <josue@tille.ch>
2025-09-05 00:00:04 +0200

    Remove YNH_CORE_SCRIPT env var

--- src/backup.py
+++ src/backup.py
@@ -617,7 +617,6 @@ class BackupManager:
 
         # Prepare environnement
         env_dict = self._get_env_var()
-        env_dict["YNH_CORE_SCRIPT"] = "1"
 
         # Actual call to backup scripts/hooks
 
@@ -1236,7 +1235,6 @@ class RestoreManager:
         env_dict = {
             "YNH_BACKUP_DIR": self.work_dir,
             "YNH_BACKUP_CSV": os.path.join(self.work_dir, "backup.csv"),
-            "YNH_CORE_SCRIPT": "1",
         }
         operation_logger.extra["env"] = env_dict
         operation_logger.flush()
