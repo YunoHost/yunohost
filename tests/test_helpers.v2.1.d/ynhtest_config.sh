31fd11f51b590bb9bd27f0a7281544158a9b483d
ljf <ljf+git@grimaud.me>
2025-09-23 00:34:34 +0200

    [fix] Boolean ini unit test bis

--- tests/test_helpers.v2.1.d/ynhtest_config.sh
+++ tests/test_helpers.v2.1.d/ynhtest_config.sh
@@ -185,10 +185,10 @@ EOF
     test "$(_read_ini                   "$file"       "theme")" == "\"colib'ris\""
     test "$(ynh_read_var_in_file --file="$file" --key="theme")" == "colib'ris"
 
-    test "$(_read_ini                   "$file"       "email")" == "'root@example.com'"
+    test "$(_read_ini                   "$file"       "email")" == "'root@example.com ; This is a comment without quotes'"
     test "$(ynh_read_var_in_file --file="$file" --key="email")" == "root@example.com"
 
-    test "$(_read_ini                   "$file"       "port")" == "1234"
+    test "$(_read_ini                   "$file"       "port")" == "1234 ; This is a comment without quotes"
     test "$(ynh_read_var_in_file --file="$file" --key="port")" == "1234"
 
     test "$(_read_ini                   "$file"       "url")" == "'https://yunohost.org'"
