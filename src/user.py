ca52e7da14c1057b2d79897dcbb95e019c0bfa61
Félix Piédallu <felix@piedallu.me>
2025-09-29 22:59:45 +0200

    src: Replace calls to nscd with sss_cache

--- src/user.py
+++ src/user.py
@@ -292,8 +292,7 @@ def user_create(
         raise YunohostError("user_creation_failed", user=username, error=e)
 
     # Invalidate passwd and group to take user and group creation into account
-    subprocess.call(["nscd", "-i", "passwd"])
-    subprocess.call(["nscd", "-i", "group"])
+    subprocess.call(["sss_cache", "-E"])
 
     try:
         # Attempt to create user home folder
@@ -400,7 +399,7 @@ def user_delete(
     AdminAuth.invalidate_all_sessions_for_user(username)
 
     # Invalidate passwd to take user deletion into account
-    subprocess.call(["nscd", "-i", "passwd"])
+    subprocess.call(["sss_cache", "-E"])
 
     if purge:
         subprocess.call(["rm", "-rf", f"/home/{username}"])
@@ -607,8 +606,7 @@ def user_update(
         PortalAuth.invalidate_all_sessions_for_user(username)
 
     # Invalidate passwd and group to update the loginShell
-    subprocess.call(["nscd", "-i", "passwd"])
-    subprocess.call(["nscd", "-i", "group"])
+    subprocess.call(["sss_cache", "-E"])
 
     # Trigger post_user_update hooks
     hook_callback("post_user_update", env=env_dict)
