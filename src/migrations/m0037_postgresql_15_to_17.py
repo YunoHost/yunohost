27bf0174aa49eb51f82049408ad72d825f457727
Kay0u <pierre@kayou.io>
2025-11-24 21:25:14 +0100

    migrations: Add migration script for upgrading PostgreSQL from 15 to 17

---
+++ src/migrations/m0037_postgresql_15_to_17.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2026 YunoHost Contributors
+#
+# This file is part of YunoHost (see https://yunohost.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from .postgresql import PostgreSQLMigration
+
+
+class MyMigration(PostgreSQLMigration):
+    "Migrate DBs from Postgresql 15 to 17 after migrating to Trixie"
+
+    previous_version = "15"
+    target_version = "17"
+
+    dependencies = ["migrate_to_trixie"]
