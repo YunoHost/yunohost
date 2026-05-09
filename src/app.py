0f8eba4a3f6a6b56e8ff9c89b22a57e960c4ba94
Florent <florent.git@zeteo.me>
2026-05-09 20:59:59 +0200

    fix linter

--- src/app.py
+++ src/app.py
@@ -1892,6 +1892,7 @@ def app_shell(app: str) -> None:
         env=env,
     )
 
+
 def app_db(app):
     """
     Open an interactive DB pompt for the app
@@ -1918,26 +1919,27 @@ def app_db(app):
 
     if type == "postgresql":
         password_file_content = f"localhost:5432:{database}:{user}:{password}"
-    elif type =="mysql":
+    elif type == "mysql":
         password_file_content = f"""
 [client]
 user={user}
 password={password}
 """
     else:
-        raise YunohostValidationError("app_db_prompt_type_not_supported")
-
+        raise YunohostValidationError("app_db_prompt_type_not_supported", type=type)
 
     # NOTE: Postgresql (and probably mariadb too) requires the password file to
     # be a regular one. We can't work with pipes.
     # Let's create a temp file that is destroyed when quitting
     with tempfile.NamedTemporaryFile() as fp:
         fp.write(password_file_content.encode())
-        fp.flush() # FIXME: should close + call NamedTemporaryFile with `delete_on_close=False`
-                   # when we can (requires 3.13 so when we deprecate bookworm)
+        # FIXME: should close + call NamedTemporaryFile with `delete_on_close=False`
+        # when we can (requires 3.13 so when we deprecate bookworm)
+        fp.flush()
+
         env_by_type = {
             "mysql": {},
-            "postgresql": { "PGPASSFILE": fp.name }
+            "postgresql": {"PGPASSFILE": fp.name}
         }
         command = {
             "mysql": ["mysql", f"--defaults-file={fp.name}", database],
