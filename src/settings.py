992b446678afee5888329804d3e083880b8b427c
selfhoster1312 <selfhoster1312@kl.netlib.re>
2026-09-26 15:59:08 +0200

    feat: Add security.ssh.ssh_root_pubkey_authentication setting

--- src/settings.py
+++ src/settings.py
@@ -340,6 +340,7 @@ def reconfigure_nginx_and_yunohost(setting_name, old_value, new_value):
 
 @post_change_hook("ssh_compatibility")
 @post_change_hook("ssh_password_authentication")
+@post_change_hook("ssh_root_pubkey_authentication")
 def reconfigure_ssh(setting_name, old_value, new_value):
     if old_value != new_value:
         regen_conf(names=["ssh"])
